d7bb7165f58d3dd8dccb308789443965046f0ffc
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2023-10-02 01:04:58 +0100

    FEAT: adds new file that prints out Celsius to Fahrenheit conversion
    Logic still depends on fahr
    
    However output is modified to prioritise Celsius to Fahrenheit

---
+++ book-c-programming-2nd-ed-1988/2-celsius-to-farenheit/f-to-c.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	fahr = lower;
+
+	// header
+	printf("Celsius\t\tFahrenheit\n");
+	
+	while (fahr <= upper) {
+		// non decimal point
+		// celsius = 5 * (fahr - 32) / 9;
+
+		// adds decimal point logic
+		//   formula closer to maths
+		celsius = (5.0 / 9.0) * (fahr - 32.0);
+		// prints the degrees without good formatting
+		// printf("%d\t\t%d\n", fahr, celsius);
+		
+		// prints the degrees with formatting and right indentation
+		// printf("%3d\t\t%6d\n", fahr, celsius);
+		
+		// prints out to decimal values
+		//   required because the updated formular uses decimals
+		printf("%6.1f\t\t%3.0f\n", celsius, fahr);
+
+		fahr = fahr + step;
+	}
+	return 0;
+}
